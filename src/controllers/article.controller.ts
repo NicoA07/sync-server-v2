9a37845ce34dfd7831eb441beec09bbbeb751134
Nico Agassi <ocinagassi@gmail.com>
2024-04-09 16:24:59 +0800

    implement searchArticle function from v1

--- src/controllers/article.controller.ts
+++ src/controllers/article.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param, Post, Body, Put, Delete, Query } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'; // Import decorators
+import { ApiOperation, ApiResponse, ApiTags, ApiQuery } from '@nestjs/swagger'; // Import decorators
 import { ArticleService } from '../database/article.service';
 import { Article } from '../database/schemas/article.schema';
 import { Types } from 'mongoose';
@@ -10,81 +10,66 @@ export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
   @Get()
-  @ApiOperation({ summary: 'Find all articles', description: 'Get a list of all articles' })
-  @ApiResponse({ status: 200, description: 'List of articles returned successfully' })
+  @ApiOperation({
+    summary: 'Find all articles',
+    description: 'Get a list of all articles',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'List of articles returned successfully',
+    type: Article,
+    isArray: true,
+  })
+  @ApiQuery({
+    name: 'q',
+    required: false,
+    description: 'Keyword to search in article title or tags',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    description: 'Maximum number of articles to return',
+  })
+  @ApiQuery({
+    name: 'tag',
+    required: false,
+    description: 'Hashtag to filter articles',
+  })
+  @ApiQuery({
+    name: 'category',
+    required: false,
+    description: 'Category to filter articles (政經, 國際, 社會, 科技, 環境, 生活, 運動)',
+  })
   async findAll(
     @Query('q') keyword?: string, 
-    @Query('limit') limit?: number
+    @Query('limit') limit?: number,
+    @Query('tag') tag?: string,
+    @Query('page') page?: number,
+    @Query('tbs') tbs?: string,
+    @Query('category') category?: string,
   ): Promise<Article[]> {
-    // console.log('Keyword:', keyword); // Add this line to log the keyword
-    try {
-      const articles = await this.articleService.findAll({ keyword }, { limit });
+      // Check if more than one query parameter is provided
+      const queryParams = [keyword, tag, category].filter(Boolean);
+      if (queryParams.length > 1) {
+        throw new Error('Only one of the query parameters (q, tag, category) can be provided.');
+      }
+
+      // Construct filter object based on query parameters
+      const filter = {};
+      if (keyword) {
+        // Construct filter for keyword search
+        filter['keyword'] = keyword;
+      } else if (tag) {
+        // Construct filter for hashtag search
+        filter['tag'] = tag;
+      } else if (category) {
+        // Construct filter for category search
+        filter['category'] = category;
+      }
+
+      const articles = await this.articleService.findAll(filter, { limit });
       return articles;
-    } catch (error) {
-      throw new Error('Failed to fetch articles');
     }
-    // try {
-    //   const articles = await this.articleService.findAll({ keyword }, { limit });
-    //   return {
-    //     code: 200,
-    //     type: 'success',
-    //     data: articles,
-    //   };
-    // } catch (error) {
-    //   return {
-    //     code: 500,
-    //     type: 'error',
-    //     message: 'An error occurred while fetching articles',
-    //     error: error.message,
-    //   };
-    // }
-  }
-    
-  // async findAll(@Query('q') keyword?: string, @Query('limit') limit?: number) {
-  //   const filter = keyword ? {
-  //     $or: [
-  //       { title: { $regex: keyword, $options: 'i' } },
-  //       { outline: { $regex: keyword, $options: 'i' } },
-  //     ],
-  //   } : {};
-
-  //   const options = limit ? { limit } : { limit: 30 };
-
-  //   // Call the findAll function from ArticleService
-  //   const articles = await this.articleService.findAll(filter, options);
-  //   return {
-  //     code: 200,
-  //     type: 'success',
-  //     data: articles,
-  //   };
-  // } catch (error) {
-  //   // Handle error
-  //   return {
-  //     code: 500,
-  //     type: 'error',
-  //     message: 'An error occurred while fetching articles',
-  //     error: error.message,
-  //   };
-  // }
-
-  // @Get()
-  // async findAll(@Query('q') keyword: string, @Query('limit') limit: number) {
-  //   try {
-  //     const articles = await this.articleService.findAll(keyword, limit);
-  //     return {
-  //       code: 200,
-  //       type: 'success',
-  //       data: articles,
-  //     };
-  //   } catch (error) {
-  //     return {
-  //       code: 404,
-  //       type: 'error',
-  //       message: 'No search kllll results found',
-  //     };
-  //   }
-  // }
-
 
   @Get(':id')
   async findOneByID(@Param('id') id: string): Promise<Article> {
--- src/database/article.service.ts
+++ src/database/article.service.ts
@@ -10,36 +10,76 @@ export class ArticleService {
     private readonly articleModel: Model<ArticleDocument>,
   ) {}
 
-  // async findAll(
-  //   filter?: object,
-  //   options: QueryOptions = { limit: 30 },
-  // ): Promise<Article[]> {
-  //   return this.articleModel.find(filter ?? {}, null, options).exec();
-  // }
-
   async findAll(
-    filter: { keyword?: string } = {}, 
-    options: QueryOptions = { limit: 5 }
-    ): Promise<Article[]> {
+    // Define parameters for filtering and pagination
+    filter: { 
+      keyword?: string 
+      tag?: string,
+      category?: string,
+    } = {}, 
+    options: QueryOptions = { limit: 30 }
+  ): Promise<Article[]> {
     try {
-      let query = this.articleModel.find();
-      console.log('filter:', filter, 'options:', options);
+      let query = {};
+
+      // Logging filter and options
+      console.log(
+        'filter:', 
+        '\nkeyword:', filter.keyword,
+        '\ntag:', filter.tag,
+        '\ncategory:', filter.category,
+        '\noptions:', options
+      );
+  
+      // Construct query if keyword is provided
       if (filter.keyword) {
-        query = query.where({
+        // Create a query to search for articles with matching keyword in title, outline, or tags
+        query = {
           $or: [
             { title: { $regex: filter.keyword, $options: 'i' } },
             { outline: { $regex: filter.keyword, $options: 'i' } },
           ],
-        });
+        };
+      } else if (filter.tag) {
+        query = { tags: filter.tag };
+      } else if (filter.category) {
+        query = { category: filter.category };
       }
 
+      // Count the number of articles matching the query
+      const count = await this.articleModel.countDocuments(query);
+  
+      // Logging the number of articles matched with the keyword
+      console.log('Number of articles matched with the keyword:', count);
+  
+      // Apply limit if specified
+      let finalQuery = this.articleModel.find(query);
       if (options.limit) {
-        query = query.limit(options.limit);
+        finalQuery = finalQuery.limit(options.limit);
       }
-
-      return await query.sort({ _id: -1 }).exec();
+  
+      // Sort results by _id in descending order
+      finalQuery = finalQuery.sort({ _id: -1 });
+  
+      // Execute the query and fetch articles
+      const articles = await finalQuery.exec();
+  
+      // Throw error if no articles are found
+      if (!articles || articles.length === 0) {
+        console.log('No articles found with the matching keyword!');
+      }
+  
+      // Log title and link for each fetched article
+      articles.forEach(article => {
+        console.log('Title:', article.title);
+        console.log('Link:', `https://sync.muilab.org/#/article/${article._id}`);
+      });
+  
+      // Return the fetched articles
+      return articles;
     } catch (error) {
-      throw new Error('Failed to fetch articles');
+      // Throw an error if fetching articles fails
+      throw new Error('Failed to fetch articles: ' + error.message);
     }
   }
 
